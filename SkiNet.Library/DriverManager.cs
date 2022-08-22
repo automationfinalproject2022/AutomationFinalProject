bd93849714f9130d43284652aceda1b5899166fe
GeorgievaIva <iva-georgieva@abv.bg>
2022-08-22 22:18:52 +0300

    DriverManager

--- SkiNet.Library/DriverManager.cs
+++ SkiNet.Library/DriverManager.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,29 @@ namespace SkiNet.Library
 {
     public class DriverManager
     {
+        private static IWebDriver _driver;
+        public IWebDriver Driver => _driver;
+
+        public void Start()
+        {
+            if (_driver == null)
+            {
+                _driver = new ChromeDriver();
+                LoadUrl();
+            }
+        }
+
+        private void LoadUrl(string url = null)
+        {
+            if (url == null)
+            {
+                url = Config.Configuration.Url;
+            }
+
+            Driver.Url = url;
+            Driver.Manage().Window.Maximize();
+        }
+
+        public void Quit() => Driver.Quit();
     }
 }

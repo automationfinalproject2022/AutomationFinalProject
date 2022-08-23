37b0773897250af40438ecd8f0ab125079271833
nicolay-g <ng82@mail.bg>
2022-08-23 10:13:03 +0300

    Implemented abstract Form and LoginForm classes.

---
+++ SkiNet.Library/Abstract/Form.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiNet.Library.Abstract
+{
+    public abstract class Form : PageObject
+    {
+        private IWebElement _titleElement => SearchContext.FindElement(By.CssSelector("[class*='h3']"));
+        private IWebElement _emailElement => SearchContext.FindElement(By.Id("Email Address"));
+        private IWebElement _passwordElement => SearchContext.FindElement(By.Id("Password"));
+
+        protected IWebElement ActionButton => SearchContext.FindElement(By.CssSelector("button[type='submit']"));
+
+
+        protected Form(IWebElement webElement) : base(webElement)
+        {
+
+        }
+
+        public string Title => _titleElement.Text;
+
+        public string EmailAddress => _emailElement.Text;
+
+        public string Password => _passwordElement.Text;
+
+        protected abstract void Action(IWebElement webElement);
+    }
+}
---
+++ SkiNet.Library/PageComponents/PageObjects/LoginForm.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using SkiNet.Library.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiNet.Library.PageComponents.PageObjects
+{
+    public class LoginForm : Form
+    {             
+        private IWebElement _forgotPasswordLink => SearchContext.FindElement(By.LinkText("Forgot password ?"));
+
+        public LoginForm(IWebElement webElement) : base(webElement)
+        {
+        }
+
+        protected override void Action(IWebElement webElement)
+        {
+            webElement.Click();
+        }
+
+        public void SignIn()
+        {
+            Action(ActionButton);
+        }
+
+        public void ForgotPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+         
+    }
+}

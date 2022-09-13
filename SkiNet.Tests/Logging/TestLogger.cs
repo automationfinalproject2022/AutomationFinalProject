f9ce03569a09e76b6a8593f9ee6879ae550c0653
AntonioVladimirov <tonkata936463@gmail.com>
2022-09-13 00:01:12 -0700

    Implementing logger

---
+++ SkiNet.Tests/Logging/TestLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SkiNet.Logging;
+
+namespace SkiNet.Tests.Logging
+{
+    public class TestLogger : ILogger
+    {
+        private void  Log(string prefix, string message) => TestContext.WriteLine(prefix + ": " + message);
+
+        public void Error(string message) => Log(nameof(Error), message);
+
+        public void Info(string message) => Log(nameof(Info), message);
+
+        public void Warning(string message) => Log(nameof(Warning), message);
+
+    }
+}

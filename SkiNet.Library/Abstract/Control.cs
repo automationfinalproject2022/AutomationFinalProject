1b429315b31a0b18d56be14c6ee3f7e5fa92b0ed
AntonioVladimirov <tonkata936463@gmail.com>
2022-10-03 21:20:27 +0300

    Adding Control

--- SkiNet.Library/Abstract/Control.cs
+++ SkiNet.Library/Abstract/Control.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace SkiNet.Library.Abstract
 {
-    public abstract class Control
+    public abstract class Control : PageObject
     {
+        private IWebElement NameElement => SearchContext.FindElement(By.TagName("label"));
+
+        public Control(IWebElement webElement) : base(webElement)
+        {
+        }
+
+        public string Name => NameElement.Text.Replace("*", "").Trim();
+
+        public abstract void SetData(string data);
+
+        public abstract string ActualData { get; }
     }
 }
